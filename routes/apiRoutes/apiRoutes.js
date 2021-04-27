23cd2fe57aa03b7f4b5122662823514ace10c3c1
Salome Ranson <salomeranson@gmail.com>
2021-04-27 16:31:28 -0400

    successfully refactored app.post function to add note data to api in memory - nNOT adding  data to noteData.js file

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -6,20 +6,11 @@ module.exports = (app) => {
     // app.get('/api/notes', (req, res) => res.sendFile(path.join(process.cwd(), '/data/db.json')));
     app.get('/api/notes', (req, res) => res.json(noteData));
 
-
-    
-    // app.post('/api/notes', function(req, res) {
-    //     let newNote = req.body;
-    //     var data = fs.readFileSync(process.cwd(), '/data/noteData.js');
-    //     var myObj = JSON.parse(data);
-    //     myObj.push(newNote)  
-    //     newNote = JSON.stringify(myObj);
-    //     fs.writeFile('./data/db.json', newNote, err => {
-    //         if (err) throw err;
-    //         console.log('NEW NOTE ADDED!');
-    //     }); 
-    //     res.json(newNote);
-    // })
+   app.post('/api/notes', (req, res) => {
+        let newNote = req.body;
+        noteData.push(newNote);
+        res.send('Successfully added note!');
+   })
 
 }
 
